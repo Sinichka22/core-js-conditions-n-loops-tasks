df0b42dcb66bb5cc4d7855662ee8d4a665b32ddf
Aliaksandra <a.sinitsyna07@gmail.com>
2024-02-10 23:38:42 +0100

    feat: solution to task #4

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -98,8 +98,11 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if ((a === b || b === c || c === a) && a + b > c && b + c > a && c + a > b) {
+    return true;
+  }
+  return false;
 }
 
 /**
